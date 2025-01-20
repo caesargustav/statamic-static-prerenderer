7f8ec543ccf47b196adfd3306f321481795c6ad0
Tim Kleyersburg <tk@wacg.de>
2025-01-20 15:28:44 +0100

    fix: type update

--- src/Listeners/AppendExternalDataBluetprint.php
+++ src/Listeners/AppendExternalDataBluetprint.php
@@ -4,11 +4,12 @@ namespace Caesargustav\StaticPrerenderer\Listeners;
 
 use Caesargustav\StaticPrerenderer\Blueprints\ExternalDataBlueprint;
 use Statamic\Events\EntryBlueprintFound;
+use Statamic\Events\TermBlueprintFound;
 use Statamic\Support\Str;
 
 class AppendExternalDataBluetprint
 {
-    public function handle(EntryBlueprintFound $event): void
+    public function handle(EntryBlueprintFound|TermBlueprintFound $event): void
     {
         // We don't want the SEO fields to get added to the blueprint editor
         if (Str::contains(request()->url(), '/blueprints/')) {
